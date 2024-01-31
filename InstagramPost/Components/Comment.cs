299a29bf451920f91535833226999dc206a5bfd6
MehulPawar <MehulPawar@DESKTOP-QLO065D>
2024-01-31 15:58:12 +0530

    like,dislike,DeleteComent  implimented

--- InstagramPost/Components/Comment.cs
+++ InstagramPost/Components/Comment.cs
@@ -8,15 +8,21 @@ namespace InstagramPost.Components
 {
     internal class Comment
     {
+        #region Properties
+        Post OriginalPost { get; set; }
         String CommentText {  get; set; }
 
+        int CommentID { get; set; }
+        int Hierarchy {  get; set; }
         int Like {  get; set; }
         int DisLike { get; set; }
 
         bool IsDeleted { get; set; }
         bool HasReply { get; set; }
 
-        Comment Reply { get; set; }
+        List<Comment> Replies { get; set; }
+
+        #endregion
 
         public Comment() 
         { 
@@ -24,8 +30,11 @@ namespace InstagramPost.Components
             HasReply = false;
             Like = 0;   
             DisLike = 0;
+            Replies=new List<Comment>();
+            
         }    
 
+       
         public Comment(String CommentText)
         {
             this.CommentText = CommentText;
@@ -33,13 +42,65 @@ namespace InstagramPost.Components
             HasReply = false;
             Like = 0;
             DisLike = 0;
+            Replies = new List<Comment>();
         }
+
+        public Comment(String CommentText,Post OriginalPost,int Hierarchy)
+        {
+            this.CommentText = CommentText;
+            this.OriginalPost = OriginalPost;   
+            IsDeleted = false;
+            HasReply = false;
+            Like = 0;
+            DisLike = 0;
+            this.Hierarchy = Hierarchy;
+            Replies = new List<Comment>();
+            CommentID = OriginalPost.GetTotalComments()-1;
+        }
+
+        #region Get Value mathods
+        public int GetCommentID() { return CommentID; }
+
+        public bool GetHasReply() {  return HasReply; }
+
+        public bool GetIsDeleted() { return IsDeleted; }
+        #endregion
 
         #region PrintComment
         public void PrintComment() 
         {
-            Console.WriteLine(CommentText+"\t\t\tLike : "+this.Like+"\tDisLike : "+this.DisLike);
+            
+            for (int i = 0; i < Hierarchy; i++) 
+                 Console.Write("\t"); 
+
+            if(IsDeleted)
+            {
+                String DeleteText = "This comment is Deleted";
+
+                Console.WriteLine(DeleteText);
+            }
+            else
+            {
+                Console.WriteLine(String.Format("({0}) {1} \t\t\t Like : {2} \tDisLike : {3}", CommentID, CommentText, this.Like, this.DisLike));
+            }
         }
+        #endregion
+
+        #region Delete values ofComment
+        public void DeleteValues() 
+        { 
+            Like=0; DisLike=0; IsDeleted=true; CommentText = null;
+        }
+        #endregion
+
+        #region Reply of Comment
+
+        public void ReplyofComment(Comment Reply)
+        {
+            Replies.Add(Reply);
+        }
+
+
         #endregion
     }
 }
--- InstagramPost/Components/Post.cs
+++ InstagramPost/Components/Post.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace InstagramPost.Components
 {
@@ -10,78 +12,277 @@ namespace InstagramPost.Components
     {
         String Link { get; set; }
 
-        String Media {  get; set; }
+        String Media { get; set; }
 
         int Likes { get; set; }
+        bool IsLiked { get; set; }
+        bool IsDisLiked { get; set; }
 
-        int TotalComment {  get; set; } 
-        
+        int TotalComments { get; set; }
+
         int DisLikes { get; set; }
 
         List<Comment> Comments { get; set; }
 
-        public Post() 
+        Dictionary<int,Comment> CommentsWithID { get; set; }
+
+        public Post()
         {
-           Comments = new List<Comment>(); 
-           Likes = 0;
+            Comments = new List<Comment>();
+            Likes = 0;
             DisLikes = 0;
+            IsLiked = false;
+            TotalComments = 0;
         }
 
         public Post(String media)
         {
             this.Media = media;
             Likes = 0;
+            IsLiked=false;  IsDisLiked = false;
             Comments = new List<Comment>();
+            CommentsWithID=new Dictionary<int,Comment>();
+            TotalComments = 0;
         }
 
         #region DisplayPost
         public void DisplayPost()
         {
-            Console.WriteLine("--------------------------------------------------------------\n");
-            Console.WriteLine("\tPost : "+this.Media);
-            Console.WriteLine("\tTotal Likes : "+this.Likes);
-            Console.WriteLine("\tTotal DisLikes : " + this.DisLikes);
-            Console.WriteLine("\nComments : "+TotalComment+"\n");
+            Console.WriteLine("----------------------------------------------------------------------------------------------------\n\n");
+            Console.WriteLine("\t\t\t\tPost : " + this.Media);
+            Console.WriteLine("\t\t\t\tTotal Likes : " + this.Likes);
+            Console.WriteLine("\t\t\t\tTotal DisLikes : " + this.DisLikes);
+            Console.WriteLine("\n\nComments : " + TotalComments + "\n");
 
-            foreach (Comment comment in Comments) 
-            { 
+            foreach (Comment comment in Comments)
+            {
                 comment.PrintComment();
             }
 
-            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("----------------------------------------------------------------------------------------------------");
 
+            Reaction();
 
-            Console.Write("Add Comment : ");
+        }
+        #endregion
+
+        #region Decision for Reaction
+        public int Decision()
+        {
+            Console.WriteLine("0. Exit\t 1. Add Comment\t 2. Add Reply");
+            Console.WriteLine("3. Like Post\t 4.DesLike Post\t5. DeleteComment");
+            Console.Write("Select Your Decision [0-5] : ");
+
+            int decisionIndex = -1;
             try
             {
-               String comment = Console.ReadLine();
-               if(comment != "") 
-               {
-                    AddComment(GenerateComment(comment));
-                    TotalComment++;
-                    Console.Clear();
-                    DisplayPost(); 
-               }
+                decisionIndex = Int32.Parse(Console.ReadLine());
             }
-            catch(Exception e) 
-            { 
-                Console.WriteLine(e.Message);
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n"+ex.Message+"\n");
+                return Decision();
             }
 
+            if (decisionIndex < 0 || decisionIndex > 5)
+            {
+                    Console.WriteLine("\nYou Enter Wrong Choice, try again......\n");
+                    return Decision();
+            }
+
+
+            return decisionIndex;
+        }
+        #endregion
+
+        #region Reaction
+        public void Reaction()
+        {
+            int decisionIndex = Decision();
+
+            switch (decisionIndex)
+            {
+                case 1:
+                        AddComment(GenerateComment());   break;
+                case 2:
+                        AddReply(); break;
+                case 3:
+                        Like();     break;
+                case 4:
+                        DisLike();  break;
+                case 5:
+                        DeleteComment();   break;
+            }
+
+            if (decisionIndex != 0)
+            {
+                Console.Clear();
+                DisplayPost();
+            }
+
+        }
+        #endregion
+
+        #region Add Reply
+        
+        public void AddReply()
+        {
+            int CommentId = 0;
+
+            Console.Write("Enter CommentId : ");
+            try
+            {
+                CommentId = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                AddReply();
+            }
+
+            Comment comment=new Comment();
+            if(comment==null)
+            {
+                Console.WriteLine("hello");
+            }
+            Console.ReadLine();
+        }
+        #endregion
+
+        #region DeleteComment
+
+        public void DeleteComment()
+        {
+            int CommentId = 0;
+
+            Console.Write("Enter CommentId : ");
+            try
+            {
+                CommentId = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                DeleteComment();
+            }
+                
+                if (!(CommentsWithID.ContainsKey(CommentId))) { Console.WriteLine("Comment Not Exist."); Console.ReadLine(); }
+                else
+                {
+                
+                    Comment comment = CommentsWithID[CommentId];
+
+                    if(comment.GetIsDeleted())
+                    { 
+                        Console.WriteLine("Comment allready Deleted");
+                        Console.ReadLine();
+                    }
+             
+                    else if(comment.GetHasReply()) 
+                    {
+                        comment.DeleteValues();                       
+                    }
+                    else
+                    {
+                        Comments.Remove(comment);
+                        TotalComments--;
+                    }
+
+                }
+
+        }
+
+        #endregion
+
+        #region Get value Methods
+        public int GetTotalComments()
+        {
+            return this.TotalComments;
+        }
+        #endregion
+
+        #region FindCommentIndex 
+        public int FindCommentIndex(int CommentId)
+        {
+           for(int index=0; index < Comments.Count; index++) 
+            {
+                if (Comments[index].GetCommentID()==CommentId)
+                {
+                    return index;
+                }
+            }
+
+            return -1;
         }
         #endregion
 
         #region GenerateComment
-        public Comment GenerateComment(String CommentText)
+        public Comment GenerateComment()
         {
-            return new Comment(CommentText);
+            String CommentText = "Initial Comment";
+            Console.Write("Your Comment : ");
+
+            try
+            {
+                CommentText = Console.ReadLine();
+                TotalComments++;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return new Comment(CommentText, this, 1);
+
         }
         #endregion
-
+        
         #region AddComment
         public void AddComment(Comment comment)
         {
             this.Comments.Add(comment);
+            CommentsWithID.Add(comment.GetCommentID(), comment);
+        }
+        #endregion
+
+        #region Like Post
+        public void Like()  
+        {
+            //Likes++;
+            
+            if (!IsLiked)
+            {
+                Likes++;
+                IsLiked = true;
+
+            } 
+
+            if(IsDisLiked)
+            {
+                DisLikes--;
+                IsDisLiked = false;
+            }
+            
+        
+        }
+        #endregion
+
+        #region DisLike Post
+        public void DisLike() 
+        {
+            //DisLikes++;
+            
+            if (!IsDisLiked)
+            {
+                DisLikes++;
+                IsDisLiked = true;
+            }
+            if(IsLiked)
+            {
+                Likes--;
+                IsLiked = false;
+
+            }  
         }
         #endregion
 
